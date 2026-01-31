4b161cfad8e7065aa1ef3bc80158095aa4e495a3
abaste89 <a.baste@pm.me>
2026-01-31 14:00:53 +0100

    Create sites.js

---
+++ sites.js
@@ -0,0 +1,32 @@
+// sites.js
+window.SITES = [
+  // Art/Theory
+  "e-flux.com",
+  "artforum.com",
+  "artreview.com",
+  "textezurkunst.de",
+  "spikeartmagazine.com",
+  "greyroom.org",
+  "brooklynrail.org",
+  "tank.tv",
+  "cabinetmagazine.org",
+  "tripleampersand.org",
+
+  // Lit/Essay
+  "thebaffler.com",
+  "granta.com",
+  "thewhitereview.org",
+  "stingingfly.org",
+  "thedriftmag.com",
+  "spamzine.co.uk",
+
+  // Poetry
+  "thediagram.com",
+  "poetryfoundation.org",
+  "asymptotejournal.com",
+  "blackboxmanifold.sites.sheffield.ac.uk",
+
+  // Ecology
+  "ambientreceiver.org",
+  "emergencemagazine.org"
+];
